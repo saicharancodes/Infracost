8ceaa340bb0b505d15353683360657c1220e7b82
saicharan <saicharan2001t@gmail.com>
2025-03-24 05:41:50 +0000

    Auto commit at Mon Mar 24 05:41:50 GMT 2025

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the Google Cloud Provider
+# Configure the Google Cloud provider
 terraform {
   required_providers {
     google = {
@@ -8,33 +8,107 @@ terraform {
   }
 }
 
-provider "google" {
-  project = "<YOUR_PROJECT_ID>" # Replace with your GCP project ID
-  region  = "us-central1"       # Replace with your desired region
+# Define variables
+variable "project_id" {
+  type = string
+  description = "The GCP project ID"
 }
 
-
-# Create a Google Cloud Storage Bucket
-resource "google_storage_bucket" "default" {
-  name          = "<YOUR_BUCKET_NAME>" # Replace with a unique bucket name
-  location      = "US"
-  force_destroy = true # Allows deletion of non-empty buckets
+variable "environment" {
+  type = string
+  description = "The environment (e.g., dev, prod)"
 }
 
-# Create a Google Compute Engine Instance (VM)
-resource "google_compute_instance" "default" {
-  name         = "default-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+variable "location" {
+  type = string
+  default = "europe-west1"
+  description = "The location for the bucket"
+}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
+variable "storage_class" {
+  type = string
+  default = "STANDARD"
+  description = "The storage class for the bucket"
+}
+
+variable "read_write_access" {
+  type = list(string)
+  description = "List of service accounts with read/write access"
+}
+
+variable "read_only_access" {
+  type = list(string)
+  description = "List of service accounts with read-only access"
+  default = []
+}
+
+# Construct bucket name
+locals {
+  bucket_name = "out-dlm-is-${var.environment}"
+}
+
+# Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "bucket" {
+  name          = local.bucket_name
+  project       = var.project_id
+  location      = var.location
+  storage_class = var.storage_class
+  force_destroy = true
+
+  labels = {
+    data-classification = "is"
+    pii_included      = "no"
+    crop_number       = "NA"
+    bucket_type       = "data"
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 90
+    }
+    action {
+      type = "Delete"
     }
   }
 
-  network_interface {
-    network = "default" # Use the default VPC network
-    access_config {}    # Create a default external IP
+  lifecycle_rule {
+    condition {
+      age              = 7
+      storage_class    = ["NEARLINE"]
+
+    }
+    action {
+      type = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
   }
+
+
+   lifecycle_rule {
+    condition {
+      age              = 30
+    }
+    action {
+      type = "SetStorageClass"
+      storage_class = "ARCHIVE"
+    }
+  }
+
+
+}
+
+# Grant read/write access to the bucket
+resource "google_storage_bucket_iam_member" "read_write" {
+  for_each = toset(var.read_write_access)
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${each.value}"
+}
+
+# Grant read-only access to the bucket
+resource "google_storage_bucket_iam_member" "read_only" {
+  for_each = toset(var.read_only_access)
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${each.value}"
 }
